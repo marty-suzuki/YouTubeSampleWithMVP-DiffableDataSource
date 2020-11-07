d7e45e3c3cf23ba0b66f526aff9976aedbf2fb00
marty-suzuki <s1180183@gmail.com>
2020-11-08 01:08:44 +0900

    Replace LoadingView code layout to xib

--- YouTubeSample/Common/LoadingView.swift
+++ YouTubeSample/Common/LoadingView.swift
@@ -1,5 +1,5 @@
 //
-//  LoadingViewCell.swift
+//  LoadingView.swift
 //  YouTubeSample
 //
 //  Created by marty-suzuki on 2020/10/28.
@@ -8,30 +8,9 @@
 import Reusable
 import UIKit
 
-final class LoadingView: UIView, ReusableView {
+final class LoadingView: UIView, ReusableNibView {
 
-    private let indicatorView:  UIActivityIndicatorView = {
-        let indicatorView = UIActivityIndicatorView(style: .large)
-        indicatorView.color = .gray
-        indicatorView.translatesAutoresizingMaskIntoConstraints = false
-        return indicatorView
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        addSubview(indicatorView)
-        NSLayoutConstraint.activate([
-            indicatorView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            indicatorView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            indicatorView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
-            indicatorView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12)
-        ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    @IBOutlet private weak var indicatorView:  UIActivityIndicatorView!
 
     func prepareForReuse() {
         stopAnimation()
